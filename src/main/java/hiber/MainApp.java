988601a46193814fee9495e507eaa4755acae40c
fedor.teslyuk <fedor.teslyuk>
2020-08-31 23:16:53 +0500

    bad work with two bases

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -1,7 +1,9 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
+import hiber.service.CarService;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -9,26 +11,48 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class MainApp {
-   public static void main(String[] args) throws SQLException {
-      AnnotationConfigApplicationContext context = 
-            new AnnotationConfigApplicationContext(AppConfig.class);
+    public static void main(String[] args) throws SQLException {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
 
-      UserService userService = context.getBean(UserService.class);
+        UserService userService = context.getBean(UserService.class);
+        CarService carService = context.getBean(CarService.class);
+        User tmpUser = new User("User1", "Lastname1", "user1@mail.ru");
+        tmpUser.setCar(new Car());
+        tmpUser.getCar().setSeries(1);
+        userService.add(tmpUser);
+//
+//        tmpUser = new User("User2", "Lastname2", "user2@mail.ru", new Car());
+////        tmpUser.getCar().setUser(tmpUser);
+//        tmpUser.getCar().setSeries(2);
+//        userService.add(tmpUser);
+//
+//        tmpUser = new User("User3", "Lastname3", "user3@mail.ru", new Car());
+////        tmpUser.getCar().setUser(tmpUser);
+//        tmpUser.getCar().setSeries(3);
+//        userService.add(tmpUser);
+//
+//        tmpUser = new User("User4", "Lastname4", "user4@mail.ru", new Car());
+////        tmpUser.getCar().setUser(tmpUser);
+//        tmpUser.getCar().setSeries(4);
+//        userService.add(tmpUser);
+//        userService.add(new User("User4", "Lastname4", "user4@mail.ru", new Car()));
+//        List<Car> cars = carService.listCars();
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
 
-      List<User> users = userService.listUsers();
-      for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
-         System.out.println();
-      }
+        List<User> users = userService.listUsers();
+        for (User user : users) {
+            System.out.println("Id = " + user.getId());
+            System.out.println("First Name = " + user.getFirstName());
+            System.out.println("Last Name = " + user.getLastName());
+            System.out.println("Email = " + user.getEmail());
+            System.out.println("Car = " + user.getCar());
+            System.out.println();
+        }
 
-      context.close();
-   }
+
+        User a = userService.getUserBySeriesAndId(1L, 1);
+
+        context.close();
+    }
 }
---
+++ src/main/java/hiber/model/Car.java
@@ -0,0 +1,70 @@
+package hiber.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String firstName;
+
+    @Column(name = "series")
+    private Integer series;
+
+    @OneToOne(mappedBy = "cars",cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Car() {
+    }
+
+    public Car(String firstName, Integer series) {
+        this.firstName = firstName;
+        this.series = series;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public Integer getSeries() {
+        return series;
+    }
+
+    public void setSeries(Integer series) {
+        this.series = series;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "firstName='" + firstName + '\'' +
+                ", series=" + series +
+                '}';
+    }
+}
